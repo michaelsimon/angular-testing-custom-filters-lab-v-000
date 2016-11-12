9c527254c5a7f57b8370c3ce3e688a4cb8d828a2
michaelsimon <michael.simon@gmail.com>
2016-11-12 17:17:27 -0500

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -4,8 +4,18 @@ describe('Favorite Food Filter', function () {
 	beforeEach(module('app'));
 
 	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
+			$filter = $injector.get('$filter');
 	}));
 
-	
+
+	it('should filter on a favorite food', function() {
+		var mockedList = [
+			{name: 'Michael', favoriteFood: 'Chicken Wrap'},
+			{name: 'Jordan', favoriteFood: 'Pizza'},
+			{name: 'Barbara', favoriteFood: 'Chocolate'}];
+
+		var results = $filter('favoriteFood')(mockedList, 'Pizza');
+		expect(results.length).toBe(1);
+		expect(results[0].name).toBe('Jordan');
+	});
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -1,10 +1,14 @@
 describe('removeAllVowels Filter', function () {
-	var $controller;
+	var $controller, removeAllVowels;
 
 	beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
+	beforeEach(inject(function ($filter) {
+		removeAllVowels = $filter('removeAllVowels');
 	}));
+
+	it('should remove all vowels from the string', function() {
+		expect(removeAllVowels('this is a very long string with a lot of vowels')).toEqual('ths s  vry lng strng wth  lt f vwls');
+	});
 
 });
